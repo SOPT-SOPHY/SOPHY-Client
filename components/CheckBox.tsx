9925ca7f3e7db7fadb0b46e8a783afa90827be0b
yehyeon son <dara0615@ewhain.net>
2023-07-01 02:49:55 +0900

    [ feat ] : 체크박스 전체동의 부분동의 구현

---
+++ components/CheckBox.tsx
@@ -0,0 +1,80 @@
+import React, { useState, ChangeEvent } from 'react';
+
+interface Agreeds {
+  infoConfirm: boolean;
+  serviceConfirm: boolean;
+}
+
+const CheckBox = () => {
+  const [allAgreed, setAllAgreed] = useState(false);
+  const [agreeds, setAgreeds] = useState<Agreeds>({
+    infoConfirm: false,
+    serviceConfirm: false,
+  });
+
+  const handleAgreedChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    setAgreeds((prevAgreeds) => ({ ...prevAgreeds, [name]: checked }));
+
+    const allChecked = Object.values({ ...agreeds, [name]: checked }).every(
+      (value) => value === true,
+    );
+    setAllAgreed(allChecked);
+  };
+
+  const handleAllAgreedChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { checked } = event.target;
+    setAgreeds({
+      infoConfirm: checked,
+      serviceConfirm: checked,
+    });
+    setAllAgreed(checked);
+  };
+
+  return (
+    <>
+      <label>이용약관 동의</label>
+      <ul>
+        <li>
+          <input
+            type="checkbox"
+            id="agree_check_all"
+            name="agree_check_all"
+            checked={allAgreed}
+            onChange={handleAllAgreedChange}
+          />
+          <label htmlFor="agree_check_all">모두 동의합니다.</label>
+        </li>
+        <li>
+          <input
+            type="checkbox"
+            id="agree_check_info"
+            name="infoConfirm"
+            required
+            checked={agreeds.infoConfirm}
+            onChange={handleAgreedChange}
+          />
+          <label htmlFor="agree_check_info">
+            북토크 정보 및 장소를 확인하였습니다.
+          </label>
+        </li>
+        <li>
+          <input
+            type="checkbox"
+            id="agree_check_service"
+            name="serviceConfirm"
+            required
+            checked={agreeds.serviceConfirm}
+            onChange={handleAgreedChange}
+          />
+          <label htmlFor="agree_check_service">
+            사전 연락 없이 무단으로 불참할 경우 추후 서비스 이용에 제약이 있을
+            수 있음을 확인하였습니다.
+          </label>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default CheckBox;
--- pages/yeson/btApply.tsx
+++ pages/yeson/btApply.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ToastCom from '../../components/ToastCom';
+import CheckBox from '../../components/CheckBox';
 
 const Apply = () => {
   return (
     <>
       <div>toastpractice</div>
+      <CheckBox />
       <ToastCom />
     </>
   );
