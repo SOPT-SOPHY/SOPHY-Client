a42229a492ae592aded0757de368f5154419ccde
yehyeon son <dara0615@ewhain.net>
2023-07-01 01:50:58 +0900

    [ feat ] : btApply 페이지 내 토스트 연결

---
+++ pages/yeson/btApply.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToastCom from '../../components/ToastCom';
+
+const Apply = () => {
+  return (
+    <>
+      <div>toastpractice</div>
+      <ToastCom />
+    </>
+  );
+};
+
+export default Apply;
