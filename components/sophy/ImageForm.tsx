20c3a50bdc078f81062a514f1fc12cc1d3931f59
KwonET <dlxl3652zz@gmail.com>
2023-07-05 14:52:13 +0900

    [ feat ] : 이미지 formData 생성 완료

--- components/sophy/ImageForm.tsx
+++ components/sophy/ImageForm.tsx
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
-
+import Image from 'next/image';
 const ImageForm = () => {
   const [imageSource, setImageSource]: any = useState(null);
-  const handleUpload = (e: any) => {
-    const file = e.target.files[0];
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    if (!e.target.files) return;
+
+    const uploadFile = e.target.files[0];
+    const formData = new FormData();
+    formData.append('files', uploadFile);
+    console.log(formData);
+
+    // 서버 axios로 전달
+    //   await axios({
+    //     method: 'post',
+    //     url: '/api/files/images',
+    //     data: formData,
+    //     headers: {
+    //       'Content-Type': 'multipart/form-data',
+    //     },
+    //   });
+
     const reader = new FileReader();
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(uploadFile);
 
     return new Promise<void>((resolve) => {
       reader.onload = () => {
         setImageSource(reader.result || null); // 파일의 컨텐츠
-        console.log(reader.result);
 
         resolve();
       };
@@ -19,7 +36,9 @@ const ImageForm = () => {
   return (
     <>
       <input accept="image/*" type="file" onChange={(e) => handleUpload(e)} />
-      <img src={imageSource} />
+      {imageSource && (
+        <Image src={imageSource} alt="업로드 이미지" height={200} width={200} />
+      )}
     </>
   );
 };
