fbcfb1704db734325733c65d970551b8d66ca478
yehyeon son <dara0615@ewhain.net>
2023-07-08 17:38:21 +0900

    [ feat ] : 컬러 스타일 타입 지정

--- styles/styled.d.ts
+++ styles/styled.d.ts
@@ -5,6 +5,34 @@ declare module 'styled-components' {
     colors: {
       blackOpacity70: string;
       blackOpacity80: string;
+
+      black: string;
+      white: string;
+
+      green01: string;
+      green02: string;
+      green03: string;
+      green04: string;
+      green05: string;
+      green06: string;
+      green07: string;
+      green08: string;
+      green09: string;
+      green10: string;
+      green11: string;
+
+      gray01: string;
+      gray02: string;
+      gray03: string;
+      gray04: string;
+      gray05: string;
+      gray06: string;
+      gray07: string;
+      gray08: string;
+      gray09: string;
+      gray10: string;
+      gray11: string;
+      gray12: string;
     };
   }
 }
