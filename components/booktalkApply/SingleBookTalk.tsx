8fb3dfdafaddb4993f9c7bb2b2b66726d8d96dc2
yehyeon son <dara0615@ewhain.net>
2023-07-09 19:45:15 +0900

    [ feat ] : 북토크 리스트 컴포넌트 분리 및 라우팅

---
+++ components/booktalkApply/SingleBookTalk.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
+import icPeople from '../../assets/icon/ic_people_count.svg';
+
+interface CountryProps {
+  alpha3Code: string;
+  flag: { large: string };
+  name: string;
+  population: number;
+  region: string | string[];
+  capital: string;
+}
+
+interface SingleBookTalkProps {
+  item: CountryProps;
+}
+
+function SingleBookTalk({ item }: SingleBookTalkProps) {
+  return (
+    <>
+      <BookTalkWrapper>
+        <ImageContainer>
+          {/* <Image src={item.flag.large} width={99} height={99} alt="국가 이미지" /> */}
+        </ImageContainer>
+        <TextWrapper>
+          <BookName>{item.name}</BookName>
+          <WriterName>{item.capital}</WriterName>
+          <Date>{item.population}</Date>
+          <Space>
+            {Array.isArray(item.region) ? item.region.join(', ') : item.region}
+          </Space>
+        </TextWrapper>
+        <CountWrapper>
+          <Image src={icPeople} width={24} height={24} alt="사람" />
+          <Count>3/6</Count>
+        </CountWrapper>
+      </BookTalkWrapper>
+      <HorizontalLine />
+    </>
+  );
+}
+
+export default SingleBookTalk;
+
+// 나머지 스타일 컴포넌트 코드 생략
+
+const BookTalkWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  position: relative;
+
+  margin-top: 1.7rem;
+  margin-bottom: 1.3rem;
+  margin-left: 2rem;
+  margin-right: 2rem;
+`;
+
+const ImageContainer = styled.div`
+  width: 9.9rem;
+  height: 9.9rem;
+  flex-shrink: 0;
+
+  margin-right: 1.5rem;
+
+  border-radius: 10px;
+  background: #f4f4f4;
+
+  img {
+    width: 9.9rem;
+    height: 9.9rem;
+  }
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const BookName = styled.h1`
+  margin-bottom: 0.3rem;
+
+  ${({ theme }) => theme.fonts.subhead3_semibold};
+  color: ${({ theme }) => theme.colors.gray01};
+`;
+
+const WriterName = styled.h2`
+  margin-bottom: 1.9rem;
+
+  ${({ theme }) => theme.fonts.body3_medium};
+  color: ${({ theme }) => theme.colors.gray01};
+`;
+
+const Date = styled.h3`
+  margin-bottom: 0.5rem;
+
+  ${({ theme }) => theme.fonts.body3_regular};
+  color: ${({ theme }) => theme.colors.gray04};
+`;
+
+const Space = styled.h3`
+  ${({ theme }) => theme.fonts.body3_regular};
+  color: ${({ theme }) => theme.colors.gray04};
+`;
+
+const CountWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  position: absolute;
+
+  top: 8rem;
+  left: 28.8rem;
+
+  margin-right: 2rem;
+
+  img {
+    width: 2.4rem;
+    height: 2.4rem;
+    flex-shrink: 0;
+  }
+`;
+
+const Count = styled.h1`
+  ${({ theme }) => theme.fonts.subhead4_semibold};
+  color: ${({ theme }) => theme.colors.primary};
+
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 22px;
+  letter-spacing: -0.408px;
+`;
+
+const HorizontalLine = styled.hr`
+  margin-left: 2.2rem;
+  margin-right: 2rem;
+
+  border-top: 1px solid ${({ theme }) => theme.colors.gray11};
+`;
---
+++ pages/auth/booktalkApply/BTList.tsx
@@ -0,0 +1,185 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+// import btnUp from '../../../assets/icon/btn_up.svg';
+import { useQuery } from 'react-query';
+import axios, { AxiosError } from 'axios';
+import Link from 'next/link';
+import btnDown from '../../../assets/icon/btn_down.svg';
+import SingleBookTalk from '../../../components/booktalkApply/SingleBookTalk';
+import backArrow from '../../../assets/icon/ic_backArrow.svg';
+
+interface CountryProps {
+  alpha3Code: string;
+  flag: { large: string };
+  name: string;
+  population: number;
+  region: string | string[];
+  capital: string;
+}
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get(
+      'https://raw.githubusercontent.com/iamspruce/search-filter-painate-reactjs/main/data/countries.json',
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch data');
+  }
+};
+
+function NextPage() {
+  const router = useRouter();
+  const { region } = router.query;
+
+  const {
+    data: items,
+    isLoading,
+    error,
+  } = useQuery<CountryProps[], Error | AxiosError>('countries', fetchData);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  function filterItems(
+    filteredItems: CountryProps[] | null,
+    filterParam: string,
+  ) {
+    // if (filteredItems) {
+    //   const values: CountryProps[] = [];
+    //   for (const key in filteredItems) {
+    //     if (filteredItems[key].region === filterParam) {
+    //       values.push(filteredItems[key]);
+    //     }
+    //   }
+    //   return values;
+    // }
+    // return [];
+    if (items) {
+      const values: CountryProps[] = [];
+      Object.keys(items).forEach((key: string) => {
+        const item: CountryProps = items[key];
+        if (item.region === filterParam) {
+          values.push(item);
+        }
+      });
+      return values;
+    }
+    return [];
+  }
+
+  const filteredItems = items ? filterItems(items, region as string) : [];
+
+  return (
+    <Body>
+      <Header>
+        <Link href="/auth/booktalk/selectRegion">
+          <ImageContainer>
+            <Image src={backArrow} width={30} height={30} alt="뒤로가기" />
+          </ImageContainer>
+        </Link>
+        <Title>모집 중인 북토크</Title>
+      </Header>
+      <SelectBox>
+        <SubTitle>{region}</SubTitle>
+        <Image
+          src={btnDown}
+          width={24}
+          height={24}
+          alt="selectBox 띄우기 버튼"
+        />
+      </SelectBox>
+
+      {/* <div>
+          {filteredItems.map((item, index) => (
+            <div key={index}>
+              <p>Name: {item.name}</p>
+              <p>Population: {item.population}</p>
+            </div>
+          ))}
+        </div> */}
+      <SingleBookTalkContainer>
+        {filteredItems.map((item) => (
+          <SingleBookTalk key={item.alpha3Code} item={item} />
+        ))}
+      </SingleBookTalkContainer>
+    </Body>
+  );
+}
+
+export default NextPage;
+
+const Body = styled.div`
+  width: 37.5rem;
+
+  margin: 0 auto;
+  padding: 0;
+
+  background-color: ${({ theme }) => theme.colors.white};
+
+  border: 2px solid ${({ theme }) => theme.colors.gray11};
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+
+  position: sticky;
+  width: 37.5rem;
+  height: 4.4rem;
+  z-index: 2;
+
+  margin-top: 4.4rem;
+
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const ImageContainer = styled.div`
+  width: 4.4rem;
+  height: 4.4rem;
+
+  img {
+    width: 4.4rem;
+    height: 4.4rem;
+  }
+`;
+
+const Title = styled.h1`
+  ${({ theme }) => theme.fonts.subhead2_bold};
+  color: ${({ theme }) => theme.colors.black};
+
+  margin-right: 12.8rem;
+`;
+
+const SelectBox = styled.div`
+  display: flex;
+  align-items: center;
+
+  height: 2.4rem;
+
+  margin-top: 2.1rem;
+  margin-left: 2rem;
+  margin-bottom: 1.1rem;
+  gap: 0.2rem;
+`;
+
+const SubTitle = styled.h2`
+  ${({ theme }) => theme.fonts.subhead2_bold};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+const SingleBookTalkContainer = styled.div`
+  width: 37.5rem;
+  height: 40rem;
+
+  overflow-y: scroll;
+`;
