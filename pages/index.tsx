0ac971c10e76eb01ec945abcdf170baecf87fb94
borimong <kimhyunsu11@naver.com>
2023-07-15 18:08:28 +0900

    [ test ] : 버셀 배포 메인 머지 확인 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Landing() {
   const router = useRouter();
   return (
-    <button type="button" onClick={() => router.push('/author/region')}>
+    <button type="button" onClick={() => router.push('/auth/signup')}>
       랜딩 페이지
     </button>
   );
