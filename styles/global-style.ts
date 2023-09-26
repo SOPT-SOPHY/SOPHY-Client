1cb50619ce0b008df80ab95b7be93e7a3d971122
borimong <kimhyunsu11@naver.com>
2023-09-27 05:53:25 +0900

    fix: eliminate styled-reset

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
-import { reset } from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
-
-  ${reset}
 
     * {
     box-sizing: border-box;
